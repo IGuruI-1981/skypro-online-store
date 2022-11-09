8aff9543204658676d2eda6a9892538d1ce5ba52
Viktor <viktor.ihtiyarov@gmail.com>
2022-11-09 12:27:54 +0700

    First commit

---
+++ src/test/java/pro/sky/skypro/onlinestore/SkyproOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
